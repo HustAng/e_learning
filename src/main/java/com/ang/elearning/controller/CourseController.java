d4ffbf53795b27c40dce9d26fe0a30ad841cf896
onepiggirl <821451088@qq.com>
2017-02-28 17:20:35 +0800

    wanzheng

--- src/main/java/com/ang/elearning/controller/CourseController.java
+++ src/main/java/com/ang/elearning/controller/CourseController.java
@@ -1,6 +1,7 @@
 package com.ang.elearning.controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -10,6 +11,7 @@ import java.util.UUID;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.struts2.ServletActionContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,7 @@ import com.ang.elearning.service.ICourseService;
 import com.ang.elearning.service.IDetailService;
 import com.ang.elearning.service.ITeacherService;
 import com.ang.elearning.service.ITypeService;
+import com.sun.org.glassfish.gmbal.ParameterNames;
 
 
 @Controller
@@ -39,11 +42,35 @@ public class CourseController {
 	private IDetailService detailService;
 	@Resource
 	private ITypeService typeService;
+	@RequestMapping(value="/getAllCourse",method=RequestMethod.GET)
+	public ModelAndView getAllCourse(HttpServletRequest request)
+	{
+		ModelAndView modelAndView=new ModelAndView("houtaiCourse/CourseManage");
+		List<Course> list=courseService.findAllCourse();
+		modelAndView.addObject("courseList",list);
+		List<Teacher> teacherList=teacherService.getAllTeacher();
+		List<Detail> detailList=detailService.getALlDetail();
+		List<Type> typeList=typeService.getAllType();
+		request.getSession().setAttribute("teacherListSession",teacherList);
+		request.getSession().setAttribute("detailListSession",detailList);
+		request.getSession().setAttribute("typeListSession",typeList);
+		return modelAndView;
+	}
+	
+	
 	@RequestMapping(value="/getAllCourseByTypeId/{typeId}",method=RequestMethod.GET)
 	public ModelAndView getAllCourse(@PathVariable("typeId")int typeId)
 	{
 		ModelAndView modelAndView=new ModelAndView("Course/CourseList");
 		List<Course> list=courseService.findCourseByType(typeId);
+		modelAndView.addObject("courseList",list);
+		return modelAndView;
+	}
+	@RequestMapping(value="/getCourseByCourseName",method=RequestMethod.GET)
+	public ModelAndView getAllCourse(@RequestParam("courseName")String courseName)
+	{
+		ModelAndView modelAndView=new ModelAndView("houtaiCourse/CourseManage");
+		List<Course> list=courseService.findCourseByName(courseName);
 		modelAndView.addObject("courseList",list);
 		return modelAndView;
 	}
@@ -85,7 +112,7 @@ public class CourseController {
 		course.setTypeId(Integer.valueOf(request.getParameter("typeId")));
 		try {
 			course.setStarttime(format.parse(request.getParameter("starttime")));
-			course.setEndtime(format.parse(request.getParameter("endttime")));
+			course.setEndtime(format.parse(request.getParameter("endtime")));
 		} catch (ParseException e) {
 			System.out.println("时间转换出错");
 			e.printStackTrace();
@@ -95,7 +122,7 @@ public class CourseController {
 		course.setTeacherId(Integer.valueOf(request.getParameter("teacherId")));
 		course.setDetailId(Integer.valueOf(request.getParameter("detailId")));
 		courseService.add(course);
-		ModelAndView modelAndView=new ModelAndView("redirect:/course/BeforAddCourse");
+		ModelAndView modelAndView=new ModelAndView("redirect:/course/getAllCourse");
 		return modelAndView;
 		
 	}
@@ -110,7 +137,7 @@ public class CourseController {
 		course.setTypeId(Integer.valueOf(request.getParameter("typeId")));
 		try {
 			course.setStarttime(format.parse(request.getParameter("starttime")));
-			course.setEndtime(format.parse(request.getParameter("endttime")));
+			course.setEndtime(format.parse(request.getParameter("endtime")));
 		} catch (ParseException e) {
 			System.out.println("时间转换出错");
 			e.printStackTrace();
@@ -120,17 +147,16 @@ public class CourseController {
 		course.setTeacherId(Integer.valueOf(request.getParameter("teacherId")));
 		course.setDetailId(Integer.valueOf(request.getParameter("detailId")));
 		courseService.updateById(course);
-		ModelAndView modelAndView=new ModelAndView("redirect:/course/BeforAddCourse");
+		ModelAndView modelAndView=new ModelAndView("redirect:/course/getAllCourse");
 		return modelAndView;
 	}
 	@RequestMapping("deleteCourse")
 	public ModelAndView deleteCourseById(@RequestParam("Id") int id)
 	{
 		courseService.deleteCourse(id);
-		ModelAndView modelAndView=new ModelAndView("redirect:/course/BeforAddCourse");
+		ModelAndView modelAndView=new ModelAndView("redirect:/course/getAllCourse");
 		return modelAndView;
 	}
 	
 
 }
-
